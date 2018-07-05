b738b6b6ebe9e74fb5b402fcf30dc5498747d1a3
Kelly Janis <kellymj98@gmail.com>
2018-07-05 14:44:39 -0400

    style message and add metadata component

--- src/MessageList.js
+++ src/MessageList.js
@@ -3,9 +3,18 @@ import React from 'react'
 import Message from './Message'
 
 const MessageList = (props) => {
-    
   return (
-    <div className="MessageList">
+    <div
+      className="MessageList"
+      style={styles.messageList}
+    >
+      <div style={styles.roomAnnouncement}>
+        <h3 style={styles.h3}>
+          #general
+        </h3>
+        <p>This is the very beginning of the #general room.</p>
+      </div>
+
       {
         props.messages.map(msg => (
           <Message message={msg} key={msg.id} />
@@ -13,6 +22,23 @@ const MessageList = (props) => {
       }
     </div>
   )
+}
+
+const styles = {
+  messageList: {
+    backgroundColor: 'white',
+    flex: 1,
+    paddingBottom: '1rem',
+    overflowY: 'scroll',
+  },
+
+  roomAnnouncement: {
+    padding: '2rem 1rem',
+  },
+
+  h3: {
+    fontSize: '1.5rem',
+  },
 }
 
 export default MessageList
